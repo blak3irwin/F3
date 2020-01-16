8b76b44d6bcfb3b3066dfffba592bfc42a6edf3f
Blake Irwin <10725735+blak3irwin@users.noreply.github.com>
2020-01-16 09:25:26 -0500

    Update AO-Scraper.py
    added comments to code

--- AO-Scraper.py
+++ AO-Scraper.py
@@ -7,18 +7,24 @@ import time
 import csv
 from collections import Counter
 
+#define array for PAX
 PAX=[]
 
-#download July pages and look for FNGs
 
+#defines array for building list of urls
 urls = []
+
+#categories to find PAX
 categories=['disney','purple-cobra','any-given-sunday', 'cletus', 'crazy-ivan','downtown-train', 'ground-pounder','kennys-grave','mutiny','nutcracker','paradise-city','possum-trot','running-4-the-john','sheepdog','tea-time','the-nest','thunder','tiger-blood','to-be-determined','u-turn']
+
+#builds urls for pages of BB (10 per page), below I used 6 weeks
+weeks = 6
 for category in categories:
-    for i in range(6):
+    for i in range(weeks):
         urls.append('https://f3southwake.com/category/'+category+'/page/'+str(i+1)+'/')
     #print(urls)
 
-    
+    #loops through each page of backblasts and parses out tags and appends pax to a PAX array
     for url in urls:
         req = urllib.request.Request(url)
         #print(req)
@@ -46,15 +52,19 @@ for category in categories:
 
         except:
             print('page not found for: '+url)                
+   
     matches=[]
     matches = Counter(PAX)
     pairs = []
     for i in matches:
         pairs.append([i,matches[i]])
     print('\n\n***'+category+'**')
+    #creates CSV file for each AO with PAX counts
     f = open(category+'.csv','w')
     f.write(category+'\n\n')
     f.write('PAX,Count\n')
+
+    #prints a sorted list of PAX with counts per AO
     for i in sorted(pairs, key=lambda pax_:pax_[1],reverse=True):
         if i[0] not in ['Fitness', 'Purple Cobra', 'Disney', 'Any Given Sunday', 'Cletus', 'Crazy Ivan', 'Downtown Train', 'Ground Pounder', 'Kennys Grave', 'Mutiny', 'Nutcracker', 'Paradise City', 'Possum Trot', 'Running 4 The John', 'Sheepdog', 'Tea Time', 'The Nest', 'Thunder', 'Tiger Blood', 'To Be Determined', 'U Turn']:
             print(str(i[0])+','+str(i[1]))
